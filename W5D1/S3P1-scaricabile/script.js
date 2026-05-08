e3cc05070f8bb7f558063f101403975594142688
jacopoposer <jacoporagazzo@gmail.com>
2026-05-08 19:44:53 +0200

    Add files via upload

---
+++ W5D1/S3P1-scaricabile/script.js
@@ -0,0 +1,108 @@
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "crazySum" che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma di quei due valori, ma se il loro valore è lo stesso allora deve ritornare la loro somma moltiplicata per 3.
+*/
+
+
+
+function crazySum(num1, num2) {
+    if (num1 === num2) {
+        return (num1 + num2) * 3
+    } else {
+        return num1 + num2
+    }
+}
+
+console.log(crazySum(5, 8))
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 4000.
+*/
+
+function boundary(num) {
+    if ((num>=20 && num<=100) || num ===4000) {
+        return true
+    }
+
+}
+
+console.log(boundary(4001))
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).
+*/
+ 
+function reverseString(str) {
+    let reversed = "";
+    for (i=str.length - 1; i>=0; i--) {
+        reversed += str[i]
+    }
+    return reversed
+}
+
+console.log(reverseString("EPICODE"))
+
+
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
+*/
+
+function upperFirst(str) {
+    let revStr = str.charAt(0).toUpperCase() + str.slice(1);
+return revStr
+}
+
+console.log(upperFirst("epicode"))
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
+*/
+
+function giveMeRandom(n) {
+    let randomNumbers = [];
+    for(i=0; i<n; i++) {
+       x= Math.floor (Math.random() * 11);
+       randomNumbers.push(x);
+    }
+    return randomNumbers;
+}
+
+console.log(giveMeRandom(5))
+
+
+//EXTRA:
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
+ Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
+ La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
+ SUGGERIMENTO: operatore modulo
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
